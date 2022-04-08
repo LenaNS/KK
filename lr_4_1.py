a097a5343f3e0563ed49be88f5c25d1aa341c11a
LenaNS <71088004+LenaNS@users.noreply.github.com>
2022-04-08 22:59:50 +0800

    Update lr_4_1.py

--- lr_4_1.py
+++ lr_4_1.py
@@ -1,3 +1,4 @@
+
 class TreatmentData:
     def __init__(self, chain):
         self.chain = chain
@@ -11,32 +12,29 @@ class TreatmentData:
 class Control_table:
     stack_tokens = ""
     def proverka(self, list_tokens):
-
-        #while list_tokens != "E":
             for i in range(len(list_tokens)):
                 if "n"  == list_tokens[i]:
-                    #list_tokens = list_tokens.replace(list_tokens[i], "T")
                     self.stack_tokens += "T"
-                    print(self.stack_tokens)
-                    if "T" in self.stack_tokens:
-                        list_tokens = list_tokens.replace("T", "E")
-                        print(self.stack_tokens)
-                    if "E+T" in list_tokens:
+                    print(self.stack_tokens, "свёртка по правилу (3)")
+
+                    
+
+                    if "E+T" in self.stack_tokens:
                         self.stack_tokens = self.stack_tokens.replace("E+T", "E")
-                        print(self.stack_tokens)
-                    if "(E)" in list_tokens:
-                        self.stack_tokens = self.stack_tokens.replace("(E)", "T")
-                        print(self.stack_tokens)
+                        print(self.stack_tokens, "свёртка по правилу (1)")
+
                 else:
                     self.stack_tokens += list_tokens[i]
-
-                    print(self.stack_tokens)
-
-
+                    print(self.stack_tokens, "перенос")
+                    if "(E)" in self.stack_tokens:
+                        self.stack_tokens = self.stack_tokens.replace("(E)", "T")
+                        print(self.stack_tokens, "свёртка по правилу (4)")
+                    if "T" in self.stack_tokens and "E+T" not in self.stack_tokens :
+                        self.stack_tokens =  self.stack_tokens.replace("T", "E")
+                        print(self.stack_tokens, "свёртка по правилу (2)")
             print(self.stack_tokens)
                  
 class Analyzer:
-   
     treatment_data = TreatmentData( chain = input("Enter the chain: "))
     treatment_data.divide_tokens()
     control_table = Control_table()
