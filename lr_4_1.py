006a11b18d3b5a552a631276025c41e01dc0b2bb
LenaNS <71088004+LenaNS@users.noreply.github.com>
2022-04-08 17:49:33 +0800

    Update lr_4_1.py

--- lr_4_1.py
+++ lr_4_1.py
@@ -1,22 +1,44 @@
-
-
 class TreatmentData:
     def __init__(self, chain):
         self.chain = chain
-        self.list_tokens = []
+        self.tokens = ""
 
     def divide_tokens(self):
-        self.list_tokens = self.chain.split(" ")
+        # здесь должна быть нормальная обработка через регулярные выражения
+        self.list_tokens = self.chain
         print(f"tokens: {self.list_tokens}")
         return self.list_tokens
 
 class Control_table:
-    pravilo = ["E+T","T","n", "(E)"]
+    stack_tokens = ""
+    def proverka(self, list_tokens):
 
-class Main:
+        #while list_tokens != "E":
+            for i in range(len(list_tokens)):
+                if "n"  == list_tokens[i]:
+                    #list_tokens = list_tokens.replace(list_tokens[i], "T")
+                    self.stack_tokens += "T"
+                    print(self.stack_tokens)
+                        if "T" in self.stack_tokens:
+                            list_tokens = list_tokens.replace("T", "E")
+                            print(self.stack_tokens)
+                        if "E+T" in list_tokens:
+                             self.stack_tokens = self.stack_tokens.replace("E+T", "E")
+                            print(self.stack_tokens)
+                        if "(E)" in list_tokens:
+                                self.stack_tokens = self.stack_tokens.replace("(E)", "T")
+                                print(self.stack_tokens)
+                else:
+                    self.stack_tokens += list_tokens[i]
+
+                    print(self.stack_tokens)
+
+
+            print(self.stack_tokens)
+                 
+class Analyzer:
    
     treatment_data = TreatmentData( chain = input("Enter the chain: "))
     treatment_data.divide_tokens()
-
-
-#start = Main()
+    control_table = Control_table()
+    control_table.proverka(treatment_data.list_tokens)
